f163e5adf562e048dc5cee216ed6c339b3879c76
Sabdhagiri Govindaraju <sabdhagiri@onecloudinc.com>
2015-08-24 11:52:05 -0700

    Fixed the custom scenario plugin with proper imports

--- rally/plugins/openstack/scenarios/keystone/custom_scenario.py
+++ rally/plugins/openstack/scenarios/keystone/custom_scenario.py
@@ -1,15 +1,15 @@
-from rally.benchmark.scenarios import base
-from rally.benchmark import utils as bench_utils
-from rally.plugins.openstack.scenarios.custom import custom_utils as customutils
+from rally.plugins.openstack import scenario
+from rally.plugins.openstack.scenarios.keystone import custom_utils as csutils
 
 
-class CustomKeystone(customutils.CustomKeystoneScenario):
+
+class CustomKeystone(csutils.CustomKeystoneScenario):
     """Test plugin for creating and listing tenants"""
 
-    @base.scenario(context={})
+    @scenario.configure(context={})
     def create_tenant(self, name_length=10, **kwargs):
         tenant = self._tenant_create(name_length=name_length, **kwargs)
 
-    @base.scenario(context={})
+    @scenario.configure(context={})
     def list_tenants(self):
         self._list_tenants()
--- rally/plugins/openstack/scenarios/keystone/custom_utils.py
+++ rally/plugins/openstack/scenarios/keystone/custom_utils.py
@@ -1,17 +1,16 @@
-from rally.benchmark.scenarios import base
+from rally.plugins.openstack import scenario
+from rally.task import atomic
 import uuid
 
-class CustomKeystoneScenario(base.Scenario):
+class CustomKeystoneScenario(scenario.OpenstackScenario):
     """ Base Class for CustomKeystone scenarios with basic atomic action """
     RESOURCE_NAME_PREFIX = "rally_keystone_"
     
-    @base.atomic_action_timer("keystone.create_tenant")
+    @atomic.action_timer("keystone.create_tenant")
     def _tenant_create(self, name_length=10, **kwargs):
         name = self._generate_random_name(length=name_length)
         return self.admin_clients("keystone").tenants.create(name, **kwargs)
 
-    @base.atomic_action_timer("keystone.list_tenants")
+    @atomic.action_timer("keystone.list_tenants")
     def _list_tenants(self):
         return self.admin_clients("keystone").tenants.list()
-
- 
